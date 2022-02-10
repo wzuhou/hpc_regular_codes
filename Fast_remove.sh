1ca62f675aea1cb1b63bbaa8ace2fd7b363612d3
wzuhou <wuz1992@hotmail.com>
2022-02-10 09:53:52 +0000

    Update Fast_remove.sh

--- Fast_remove.sh
+++ Fast_remove.sh
@@ -2,4 +2,7 @@
 #Efficiently delete large directory containing thousands of files
 
 mkdir empty_dir
-rsync -a --delete empty_dir/    yourdirectory/
+#this is not recommanded, since you may erase all your data
+#rsync -a --delete empty_dir/    yourdirectory/ 
+
+find . -name "*.pdf" -print0| xargs -0 rm
