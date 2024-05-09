507f07ff43bf69b7a29230803f40610fbf1fa90c
wzuhou <wuz1992@hotmail.com>
2024-05-09 12:12:41 +0100

    Update R_R_trick.R

--- R_R_trick.R
+++ R_R_trick.R
@@ -3,6 +3,7 @@
 ord=eval(parse(text = a))
 #using variable in data name
 clfile=eval(parse(text = paste0("cl",i)))
+assign()
 
 #Join files
 plyr::test.1<-join(nrDEG,df,by="Gene",type="inner",match="all")
